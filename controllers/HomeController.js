1bf502e3105f8e0df1a6f98a2571b8444d23286e
fsenycouty <fabrice.seny-couty@oclock.school>
2026-07-20 17:54:10 +0200

    feat: test affichage dynamique de la gallerie avec dataPictures.json

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,9 +1,14 @@
 // Controller pour la page d'accueil de Peigne et Blabla
 
+// Import de dataPictures pour le test de l'affichage en dynamique de la gallerie
+import dataPictures from "../data/dataPictures.json" with { type: "json"};
+import Picture from "../models/Picture.js";
+
 class HomeController {
   // Envoye à la vue home.ejs
-  home =async (req, res) => {
-    res.render("home");
+  home = async (req, res) => {
+    // Envoi le dataPictures à la vue
+    res.render("home", { dataPictures });
   }
 }
 
